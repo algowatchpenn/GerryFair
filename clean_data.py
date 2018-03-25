cdda57869d7f245b4decba855d6980f0ec150092
sethneel <sethneel93@gmail.com>
2018-03-25 19:31:54 -0400

    clean_data

--- clean_data.py
+++ clean_data.py
@@ -11,6 +11,28 @@ import pandas as pd
 # y are the binary outcomes
 
 
+def one_hot_code(df1, sens_dict):
+    cols = df1.columns
+    for c in cols:
+        if isinstance(df1[c][0], basestring):
+            column = df1[c]
+            df1 = df1.drop(c, 1)
+            unique_values = list(set(column))
+            n = len(unique_values)
+            if n > 2:
+                for i in range(n):
+                    col_name = '{}.{}'.format(c, i)
+                    col_i = [1 if el == unique_values[i] else 0 for el in column]
+                    df1[col_name] = col_i
+                    sens_dict[col_name] = sens_dict[c]
+                del sens_dict[c]
+            else:
+                col_name = c
+                col = [1 if el == unique_values[0] else 0 for el in column]
+                df1[col_name] = col
+    return df1, sens_dict
+
+
 # num_sens in 1:18
 def clean_communities(num_sens):
     """Clean communities & crime data set."""
@@ -45,7 +67,7 @@ def clean_lawschool(num_sens):
     df = df.drop('bar1', 1)
     y = [int(a == 'P') for a in df_y]
     # one hot coding of race variable
-    for i in range(1,9):
+    for i in range(1, 9):
         col_name = 'race{}'.format(i)
         race_code = [np.int(r == i) for r in df['race']]
         df[col_name] = race_code
@@ -62,10 +84,49 @@ def clean_synthetic(num_sens):
     df = pd.read_csv('dataset/synthetic.csv')
     df = df.dropna()
     y_col = df.shape[1]-1
-    y = df.iloc[:,y_col]
-    df = df.iloc[:, 0:(y_col-1)]
+    y = df.iloc[:, y_col]
+    df = df.iloc[:, 0:y_col]
     x_prime = df.iloc[:, 0:num_sens]
     return df, x_prime, y
+
+
+# 61 possible sensitive features
+def clean_adult(num_sens):
+    df = pd.read_csv('dataset/adult.csv')
+    df = df.dropna()
+    # binarize and remove y value
+    df['income'] = df['income'].map({' <=50K': 0, ' >50K': 1})
+    y = df['income']
+    df = df.drop('income', 1)
+    # hot code categorical variables
+    sens_cols = ['marital-status', 'relationship', 'native-country', 'race', 'sex']
+    sens_dict = {c: 1 if c in sens_cols else 0 for c in df.columns}
+    df, sens_dict = one_hot_code(df, sens_dict)
+    sens_names = [key for key in sens_dict.keys() if sens_dict[key] == 1]
+    print('there are {} possible sensitive features'.format(len(sens_names)))
+    x_prime = df[sens_names[0:num_sens]]
+    return df, x_prime, y
+
+
+# currently 6 sensitive attributes
+def clean_student(num_sens):
+    sens_cols = ['sex', 'Pstatus', 'guardian', 'famrel']
+    df = pd.read_csv('dataset/student/student-mat.csv', sep=';')
+    df = df.dropna()
+    y = df['G3']
+    y = [0 if y < 11 else 1 for y in y]
+    df = df.drop(['G3', 'G2', 'G1'], 1)
+    sens_dict = {c: 1 if c in sens_cols else 0 for c in df.columns}
+    df, sens_dict = one_hot_code(df, sens_dict)
+    sens_names = [key for key in sens_dict.keys() if sens_dict[key] == 1]
+    print('there are {} possible sensitive features'.format(len(sens_names)))
+    x_prime = df[sens_names[0:num_sens]]
+    return df, x_prime, y
+
+
+
+
+
 
 
 
