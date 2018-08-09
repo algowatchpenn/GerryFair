2a18dbf23817766cb468939c41494804c1af7009
Steven Wu <steven7woo@gmail.com>
2018-08-09 17:35:28 -0400

    audit MSR code files

---
+++ audit_expgrad/clean_data.py
@@ -0,0 +1,192 @@
+import numpy as np
+import pandas as pd
+
+# documentation
+# output: clean data set, remove missing values and convert categorical values to binary, extract sensitive features
+# for each data set 'name.csv' we create a function clean_name
+# clean name takes parameter num_sens, which is the number of sensitive attributes to include
+# clean_name returns pandas data frames X, X_prime, where:
+# X is the full data set of X values
+# X_prime is only the sensitive columns of X
+# y are the binary outcomes
+
+
+def center(X):
+    for col in X.columns:
+        X.loc[:, col] = X.loc[:, col]-np.mean(X.loc[:, col])
+    return X
+
+
+def add_intercept(X):
+    """Add all 1's column to predictor matrix"""
+    X['intercept'] = [1]*X.shape[0]
+
+
+def one_hot_code(df1, sens_dict):
+    cols = df1.columns
+    for c in cols:
+        if isinstance(df1[c][0], str):
+            column = df1[c]
+            df1 = df1.drop(c, 1)
+            unique_values = list(set(column))
+            n = len(unique_values)
+            if n > 2:
+                for i in range(n):
+                    col_name = '{}.{}'.format(c, i)
+                    col_i = [1 if el == unique_values[i] else 0 for el in column]
+                    df1[col_name] = col_i
+                    sens_dict[col_name] = sens_dict[c]
+                del sens_dict[c]
+            else:
+                col_name = c
+                col = [1 if el == unique_values[0] else 0 for el in column]
+                df1[col_name] = col
+    return df1, sens_dict
+
+
+# center data frame columns for visual purposes
+def clean_communities():
+    """Clean communities & crime data set."""
+    # Data Cleaning and Import
+    df = pd.read_csv('dataset/communities.csv')
+    df = df.fillna(0)
+    y = df['ViolentCrimesPerPop']
+    q_y = np.percentile(y, 70)
+    # convert y's to binary predictions on whether the neighborhood is
+    # especially violent
+    y = [np.round((1 + np.sign(s - q_y)) / 2) for s in y]
+    X = df.iloc[:, 0:122]
+    # hot code categorical variables
+    sens_df = pd.read_csv('dataset/communities_protected.csv')
+    sens_cols = [str(c) for c in sens_df.columns if sens_df[c][0] == 1]
+    print('sensitive features: {}'.format(sens_cols))
+    sens_dict = {c: 1 if c in sens_cols else 0 for c in df.columns}
+    df, sens_dict = one_hot_code(df, sens_dict)
+    sens_names = [key for key in sens_dict.keys() if sens_dict[key] == 1]
+    print('there are {} sensitive features including derivative features'.format(len(sens_names)))
+    x_prime = df[sens_names]
+    X = center(X)
+    # X = add_intercept(X)
+    x_prime = center(x_prime)
+    # x_prime = add_intercept(x_prime)
+    return X, x_prime, pd.Series(y)
+
+
+
+
+
+# num_sens in 1:17
+def clean_lawschool():
+    """Clean law school data set."""
+    # Data Cleaning and Import
+    df = pd.read_csv('dataset/lawschool.csv')
+    df = df.dropna()
+    # convert categorical column variables to 0,1
+    df['gender'] = df['gender'].map({'female': 1, 'male': 0})
+    # remove y from df
+    df_y = df['bar1']
+    df = df.drop('bar1', 1)
+    y = [int(a == 'P') for a in df_y]
+    y = pd.Series(y)
+    sens_df = pd.read_csv('dataset/lawschool_protected.csv')
+    sens_cols = [str(c) for c in sens_df.columns if sens_df[c][0] == 1]
+    sens_dict = {c: 1 if c in sens_cols else 0 for c in df.columns}
+    # one hot coding of race variable
+    for i in range(1, 9):
+        col_name = 'race{}'.format(i)
+        if 'race' in sens_cols:
+            sens_dict[col_name] = 1
+        else:
+            sens_dict[col_name] = 0
+        race_code = [np.int(r == i) for r in df['race']]
+        df[col_name] = race_code
+    sens_dict['race'] = 0
+    df = df.drop('race', 1)
+    sens_names = [key for key in sens_dict.keys() if sens_dict[key] == 1]
+    print('there are {} sensitive features including derivative features'.format(len(sens_names)))
+    x_prime = df[sens_names]
+
+    df.index = range(len(df))
+    x_prime.index = range(len(x_prime))
+    return df, x_prime, pd.Series(y)
+
+
+def clean_synthetic(num_sens):
+    """Clean synthetic data set, all features sensitive, y value is last col."""
+    df = pd.read_csv('dataset/synthetic.csv')
+    df = df.dropna()
+    y_col = df.shape[1]-1
+    y = df.iloc[:, y_col]
+    df = df.iloc[:, 0:y_col]
+    x_prime = df.iloc[:, 0:num_sens]
+    return df, x_prime, y
+
+
+def clean_adult():
+    df = pd.read_csv('dataset/adult.csv')
+    df = df.dropna()
+    # binarize and remove y value
+    df['income'] = df['income'].map({' <=50K': 0, ' >50K': 1})
+    y = df['income']
+    df = df.drop('income', 1)
+    # hot code categorical variables
+    sens_df = pd.read_csv('dataset/adult_protected.csv')
+    sens_cols = [str(c) for c in sens_df.columns if sens_df[c][0] == 1]
+    print('sensitive features: {}'.format(sens_cols))
+    sens_dict = {c: 1 if c in sens_cols else 0 for c in df.columns}
+    df, sens_dict = one_hot_code(df, sens_dict)
+    sens_names = [key for key in sens_dict.keys() if sens_dict[key] == 1]
+    print('there are {} sensitive features including derivative features'.format(len(sens_names)))
+    x_prime = df[sens_names]
+    return df, x_prime, y
+
+
+
+
+
+
+def clean_adultshort():
+    df = pd.read_csv('dataset/adultshort.csv')
+    df = df.dropna()
+    # binarize and remove y value
+    df['income'] = df['income'].map({' <=50K': 0, ' >50K': 1})
+    y = df['income']
+    df = df.drop('income', 1)
+    # hot code categorical variables
+    sens_df = pd.read_csv('dataset/adultshort_protected.csv')
+    sens_cols = [str(c) for c in sens_df.columns if sens_df[c][0] == 1]
+    print('sensitive features: {}'.format(sens_cols))
+    sens_dict = {c: 1 if c in sens_cols else 0 for c in df.columns}
+    df, sens_dict = one_hot_code(df, sens_dict)
+    sens_names = [key for key in sens_dict.keys() if sens_dict[key] == 1]
+    print('there are {} sensitive features including derivative features'.format(len(sens_names)))
+    x_prime = df[sens_names]
+    x = center(df)
+    x_prime = center(x_prime)
+    return x, x_prime, y
+
+
+
+
+
+# currently 6 sensitive attributes
+def clean_student():
+    df = pd.read_csv('dataset/student-mat.csv', sep=';')
+    df = df.dropna()
+    y = df['G3']
+    y = [0 if y < 11 else 1 for y in y]
+    df = df.drop(['G3', 'G2', 'G1'], 1)
+    sens_df = pd.read_csv('dataset/student_protected.csv')
+    sens_cols = [str(c) for c in sens_df.columns if sens_df[c][0] == 1]
+    print('sensitive features: {}'.format(sens_cols))
+    sens_dict = {c: 1 if c in sens_cols else 0 for c in df.columns}
+    df, sens_dict = one_hot_code(df, sens_dict)
+    sens_names = [key for key in sens_dict.keys() if sens_dict[key] == 1]
+    print('there are {} sensitive features including derivative features'.format(len(sens_names)))
+    x_prime = df[sens_names]
+    return df, x_prime, pd.Series(y)
+
+
+x, a, y = clean_communities()
+a.columns
+print(len(a.columns))
---
+++ audit_expgrad/fairlearn/moments.py
@@ -0,0 +1,115 @@
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License.
+
+import pandas as pd
+
+__all__ = ["DP", "EO"]
+
+class Moment:
+    """Generic moment"""
+
+    def __init__(self):
+        self.initialized = False
+
+    def init(self, dataX, dataA, dataY):
+        assert self.initialized==False, \
+            "moments can be initialized only once"
+        self.X = dataX
+        self.tags = pd.DataFrame({"attr": dataA, "label": dataY})
+        self.n = dataX.shape[0]
+        self.initialized = True
+        self._gamma_descr = None
+
+
+class MisclassError(Moment):
+    """Misclassification error"""
+    short_name = "Err"
+
+    def init(self, dataX, dataA, dataY):
+        super().init(dataX, dataY, dataY)
+        self.index = ["all"]
+
+    def gamma(self, predictor):
+        pred = predictor(self.X)
+        error = pd.Series(data=(self.tags["label"]-pred).abs().mean(),
+                          index=self.index)
+        self._gamma_descr = str(error)
+        return error
+
+    def lambda_signed(self, lambda_vec):
+        return lambda_vec
+    
+    def signed_weights(self, lambda_vec=None):
+        if lambda_vec is None:
+            return 2*self.tags["label"]-1
+        else:
+            return lambda_vec["all"]*(2*self.tags["label"]-1)
+
+
+class _CondOpportunity(Moment):
+    """Generic fairness metric including DP and EO"""
+
+    def init(self, dataX, dataA, dataY, dataGrp):
+        super().init(dataX, dataA, dataY)
+        self.tags["grp"] = dataGrp
+        self.prob_grp = self.tags.groupby("grp").size()/self.n
+        self.prob_attr_grp = self.tags.groupby(["grp", "attr"]).size()/self.n
+        signed = pd.concat([self.prob_attr_grp, self.prob_attr_grp],
+                           keys=["+", "-"],
+                           names=["sign", "grp", "attr"])
+        self.index = signed.index
+        
+    def gamma(self, predictor):
+        pred = predictor(self.X)
+        self.tags["pred"] = pred
+        expect_grp = self.tags.groupby("grp").mean()
+        expect_attr_grp = self.tags.groupby(["grp", "attr"]).mean()
+        expect_attr_grp["diff"] = expect_attr_grp["pred"] - expect_grp["pred"]
+        g_unsigned = expect_attr_grp["diff"]
+        g_signed = pd.concat([g_unsigned, -g_unsigned],
+                             keys=["+","-"],
+                             names=["sign", "grp", "attr"])
+        self._gamma_descr = str(expect_attr_grp[["pred", "diff"]])
+        return g_signed
+
+    def lambda_signed(self, lambda_vec):
+        return lambda_vec["+"] - lambda_vec["-"]
+
+    def signed_weights(self, lambda_vec):
+        lambda_signed = lambda_vec["+"] - lambda_vec["-"]
+        adjust = lambda_signed.sum(level="grp")/self.prob_grp \
+                 - lambda_signed/self.prob_attr_grp
+        signed_weights = self.tags.apply(
+            lambda row: adjust[row["grp"], row["attr"]], axis=1
+        )
+        return signed_weights
+    
+    
+class DP(_CondOpportunity):
+    """Demographic parity"""
+    short_name = "DP"
+
+    def init(self, dataX, dataA, dataY):
+        super().init(dataX, dataA, dataY,
+                     dataY.apply(lambda y : "all"))
+
+class EO(_CondOpportunity):
+    """Equalized odds"""
+    short_name = "EO"
+    
+    def init(self, dataX, dataA, dataY):
+        super().init(dataX, dataA, dataY,
+                     dataY.apply(lambda y : "label="+str(y)))
+
+
+
+
+class FP(_CondOpportunity):
+
+    """False positive equalization"""
+    short_name = "FP"
+
+    def init(self, dataX, dataA, dataY):
+        dataA_fp = dataA.copy(deep=True)
+        dataA_fp[dataY == 0] = 'all'
+        super().init(dataX, dataA_fp, dataY, dataY.apply(lambda y : 'all'))
