5d9d4a1e8d3f288b3668c88e9b1875a38ef92ee4
Arnab Sarker <arnabs@seas.upenn.edu>
2018-10-28 17:09:03 -0400

    Changed cleaning into two phases

--- clean.py
+++ clean.py
@@ -1,5 +1,20 @@
+import argparse
 import numpy as np
 import pandas as pd
+
+def setup():
+    parser = argparse.ArgumentParser(description='Fairness Data Cleaning')
+    parser.add_argument('-n', '--name', type=str,
+                        help='name of the to store the new datasets (Required)')
+    parser.add_argument('-d', '--dataset', type=str,
+                        help='name of the original dataset file (Required)')
+    parser.add_argument('-a', '--attributes', type=str,
+                        help='name of the file representing which attributes are protected (unprotected = 0, protected = 1, label = 2) (Required)')
+    parser.add_argument('-c', '--centered', default=False, action='store_true', required=False,
+                        help='Include this flag to determine whether data should be centered')
+    args = parser.parse_args()
+    return [args.name, args.dataset, args.attributes, args.centered]
+
 
 
 '''
@@ -12,7 +27,7 @@ Attributes: Filename for the attributes of the dataset. The file should have eac
 list should have 0 for an unprotected attribute, 1 for a protected attribute, and 2 for the attribute of the label.
 
 '''
-def clean_dataset(dataset, attributes):
+def clean_dataset(dataset, attributes, centered):
     df = pd.read_csv(dataset)
     sens_df = pd.read_csv(attributes)
 
@@ -37,8 +52,10 @@ def clean_dataset(dataset, attributes):
     print('there are {} sensitive features including derivative features'.format(len(sens_names)))
 
     x_prime = df[sens_names]
-    X = center(X)
-    x_prime = center(x_prime)
+
+    if(centered):
+        X = center(X)
+        x_prime = center(x_prime)
 
     return X, x_prime, y
 
@@ -69,4 +86,34 @@ def one_hot_code(df1, sens_dict):
                 col = [1 if el == unique_values[0] else 0 for el in column]
                 df1[col_name] = col
     return df1, sens_dict
+
+
+# Helper for main method
+'''
+Given name of dataset, load in the three datasets associated from the clean.py file
+'''
+
+
+def get_data(dataset):
+    X = pd.read_csv('dataset/' + dataset + '_features.csv')
+    X_prime = pd.read_csv('dataset/' + dataset + '_protectedfeatures.csv')
+    y = pd.read_csv('dataset/' + dataset + '_labels.csv', names=['index', 'label'])
+    y = y['label']
+    return X, X_prime, y
+
+
+'''
+This file is designed to take in a raw dataset, one hot encode it, center it (if desired), and then output the clean
+data sets to be used in Reg_Oracle_Fict.py
+'''
+if __name__ == "__main__":
+    # get command line arguments
+    name, dataset, attributes, centered = setup()
+    X, X_prime, y = clean_dataset(dataset, attributes, centered)
+    X.to_csv('dataset/' + name + '_features.csv')
+    X_prime.to_csv('dataset/' + name + '_protectedfeatures.csv')
+    y.to_csv('dataset/' + name + '_labels.csv')
+
+
+
 
